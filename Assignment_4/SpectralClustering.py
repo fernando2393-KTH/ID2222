0d84d6a7f5f1e879b979f1813b52622c00b38ce6
Gustavo Beck <teodoro.beck@gmail.com>
2020-11-28 23:53:49 +0100

    Fiedler Vector and Heatmap of affinity matrix

--- Assignment_4/SpectralClustering.py
+++ Assignment_4/SpectralClustering.py
@@ -6,7 +6,10 @@ import scipy.linalg as la
 from sklearn.cluster import KMeans
 
 def load_graph(path):
-    G = nx.read_edgelist(path, delimiter=",", create_using=DiGraph)
+    if path == "data/example1.dat":
+        G = nx.read_edgelist(path, delimiter=",", create_using=DiGraph)
+    else:
+        G = nx.read_weighted_edgelist(path, delimiter=",", create_using=DiGraph)
 
     return G
 
@@ -17,8 +20,17 @@ def plot_graph(graph, labels, pos):
 def plot_eigen_values_diff(eig_values):
     plt.scatter(range(len(eig_values)), eig_values)
     plt.title("Biggest eigen values difference to find K-clusters")
-    plt.legend()
     plt.show()
+
+def plot_fiedler(f):
+    plt.plot(range(len(f)), f)
+    plt.title("Sorted Fiedler Vector")
+    plt.show()
+
+def plot_sparse(A):
+    plt.imshow(A, cmap='Blues', interpolation='nearest')
+    plt.show()
+
 
 def affinity_matrix(graph):
     '''
@@ -55,9 +67,10 @@ def eigenvector_matrix(L):
     '''
     X_values, X_vectors = la.eigh(L)
     k = compute_k(X_values)
+    fiedler_vec = sorted(X_vectors[:, 1])
     X = X_vectors[:,-k:]
 
-    return X, k, X_values
+    return X, k, X_values, fiedler_vec
 
 def normalized_X(X):
     '''
@@ -75,12 +88,15 @@ def compute_sigma():
 
 path = "data/example1.dat"
 graph = load_graph(path)
+print("Number of nodes: ", len(graph.nodes))
 pos = nx.spring_layout(graph)
 A = affinity_matrix(graph)
 L = L_matrix(A)
-X, k, eigen_values = eigenvector_matrix(L)
+X, k, eigen_values, fiedler_vec = eigenvector_matrix(L)
 Y = normalized_X(X)
 clustering = KMeans(n_clusters=k).fit(Y)
 labels = clustering.labels_
 plot_graph(graph, labels, pos)
 plot_eigen_values_diff(eigen_values)
+plot_fiedler(fiedler_vec)
+plot_sparse(A)
